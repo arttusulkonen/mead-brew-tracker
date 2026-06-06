c050fac2c8d0b7e8a1b44863e9d61f38bfe76241
Arttu <arttu@redandblue.fi>
2026-06-06 14:51:11 +0300

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { AppLayout } from './components/layout/AppLayout';
 import { getUserBreweries, processPendingInvites } from './firebase/breweryService';
 import { auth } from './firebase/config';
 import Home from './pages/Home';
 import Inventory from './pages/Inventory';
+import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 import { useAuthStore } from './store/useAuthStore';
@@ -15,7 +15,6 @@ import { useBreweryStore } from './store/useBreweryStore';
 const App: React.FC = () => {
   const { user, setUser, setLoading } = useAuthStore();
   const { setBreweries, setActiveBrewery } = useBreweryStore();
-  const { t } = useTranslation();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
@@ -57,18 +56,17 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={user ? <Navigate to="/home" replace /> : <Login />} />
         <Route path="/register" element={user ? <Navigate to="/home" replace /> : <Register />} />
         
-        <Route element={<AppLayout />}>
+        <Route element={user ? <AppLayout /> : <Navigate to="/login" replace />}>
           <Route path="/home" element={<Home />} />
-          <Route path="/recipes" element={<div style={{ padding: '2rem' }}>{t('Recipes Page')}</div>} />
-          <Route path="/brew" element={<div style={{ padding: '2rem' }}>{t('Brew Day Page')}</div>} />
-          <Route path="/journal" element={<div style={{ padding: '2rem' }}>{t('Journal Page')}</div>} />
           <Route path="/inventory" element={<Inventory />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<Navigate to="/home" replace />} />
         </Route>
 
-        <Route path="*" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
